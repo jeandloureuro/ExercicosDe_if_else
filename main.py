39f7ba347128af3c2e3d3b94cf151066e65c50bd
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-20 20:51:56 -0300

    Aula sobre if, else, exercicios feito em aula

---
+++ main.py
@@ -0,0 +1,68 @@
+
+'''
+media = int(input("Coque um valor:"))
+ if media>= 60:
+    print("Aprovado")
+else:
+  print("Reprovado")
+'''
+'''
+Ex1 =
+Velociade = int(input("Qual Sua Velocidade? "))
+Multa = ({Velociade}-80)*5
+
+if Velociade > 80:
+    print(f"Voce foi multado em R$" {Multa}." )
+'''
+'''
+EX2 = 
+a = int(input("insira um valor de a="))
+b = int(input("insira um valor de b="))
+c = int(input("insira um valor de c="))
+
+if b > a and b > c:
+     print(f"O maior valor é={a}")
+
+if b > a and b > c:
+     print(f"O maior valor é={b}")
+
+if c > a and c > b:
+     print(f"O maior valor é={c}")
+
+
+if a < b and c < b:
+     print(f"O maior valor é={b}")
+
+if b > a and c > a:
+     print(f"O maior valor é={a}")
+
+if a > c and b > c:
+     print(f"O maior valor é={c}")
+'''
+'''
+Ex3 =
+
+Salario = float(input("Seu salario"))
+
+if Salario > 1250.00:
+          print(f"Seu salario + bonus é = {(Salario*0.10) + Salario}")
+
+else:
+     print(f"Seu Salario + bonus é = {(Salario*0.15) + Salario}")
+'''
+'''
+EX4 =
+
+KM = float(input("KM Da Viagem"))
+
+if KM >= 200:
+
+    custo = (KM * 0.50 + KM )
+
+    print(f"Sua Viagem custara = {custo}")
+
+else:
+    custo = (KM * 0.45 + KM )
+
+    print(f"Sua Viagem custara = {custo}")
+'''
